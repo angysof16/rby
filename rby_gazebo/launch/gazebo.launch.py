59ae08bce55595d42231ca69b9a005506980976f
Sofia Guerra <61593038+angysof16@users.noreply.github.com>
2026-01-30 19:14:17 -0500

    Add files via upload

---
+++ rby_gazebo/launch/gazebo.launch.py
@@ -0,0 +1,354 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import (
+    AppendEnvironmentVariable,
+    DeclareLaunchArgument,
+    SetEnvironmentVariable,
+    IncludeLaunchDescription,
+    SetLaunchConfiguration,
+    RegisterEventHandler,
+)
+from launch.conditions import IfCondition
+from launch.substitutions import (
+    PathJoinSubstitution,
+    LaunchConfiguration,
+    TextSubstitution,
+)
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from xacro import process_file
+from nav2_common.launch import ReplaceString
+from launch.event_handlers import OnProcessExit
+
+ARGUMENTS = [
+    DeclareLaunchArgument(
+        "world_name",
+        default_value="empty.sdf",
+        description="Name of the world to load. Match with map if using Nav2.",
+    ),
+    DeclareLaunchArgument(
+        "ros_bridge", default_value="True", description="Run ROS bridge node."
+    ),
+    DeclareLaunchArgument(
+        "initial_pose_x",
+        default_value="0.5",
+        description="Initial x pose of rasbot in the simulation.",
+    ),
+    DeclareLaunchArgument(
+        "initial_pose_y",
+        default_value="0.0",
+        description="Initial y pose of rasbot in the simulation.",
+    ),
+    DeclareLaunchArgument(
+        "initial_pose_z",
+        default_value="0.0",
+        description="Initial z pose of rasbot in the simulation.",
+    ),
+    DeclareLaunchArgument(
+        "initial_pose_yaw",
+        default_value="0.0",
+        description="Initial yaw pose of rasbot in the simulation.",
+    ),
+    DeclareLaunchArgument(
+        "robot_description_topic",
+        default_value="robot_description",
+        description="Robot description topic.",
+    ),
+    DeclareLaunchArgument(
+        "rsp_frequency",
+        default_value="30.0",
+        description="Robot State Publisher frequency.",
+    ),
+    DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="true",
+        description="Use simulation (Gazebo) clock if true",
+    ),
+    DeclareLaunchArgument(
+        "entity", default_value="rby", description="Name of the robot"
+    ),
+]
+
+
+def get_robot_description():
+    rby_gazebo = get_package_share_directory("rby_gazebo")
+    pkg_rby_description = get_package_share_directory("rby_description")
+
+    robot_description_path = os.path.join(
+        rby_gazebo, "urdf", "robots", "rby_gazebo.urdf.xacro"
+    )
+
+    mappings = {}
+    robot_description_config = process_file(robot_description_path, mappings=mappings)
+    robot_desc = robot_description_config.toprettyxml(indent="  ")
+    robot_desc = robot_desc.replace("package://rby_description/", f"file://{pkg_rby_description}/")
+    return robot_desc
+
+
+def generate_launch_description():
+    ld = LaunchDescription(ARGUMENTS)
+
+    pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
+    rby_gazebo = get_package_share_directory("rby_gazebo")
+    pkg_rby_description = get_package_share_directory("rby_description")
+
+    gz_launch_path = PathJoinSubstitution(
+        [pkg_ros_gz_sim, "launch", "gz_sim.launch.py"]
+    )
+    world_name = LaunchConfiguration("world_name")
+    ros_bridge = LaunchConfiguration("ros_bridge")
+    world_path = PathJoinSubstitution([rby_gazebo, "worlds", world_name])
+    controller_config_path = os.path.join(
+        rby_gazebo, "config", "rby_controller_manager.yaml"
+    )
+
+    bridge_config_file_path = os.path.join(rby_gazebo, "config", "ros_gz_bridge.yaml")
+    controllers_config_file_path = os.path.join(
+        rby_gazebo, "config", "controllers.yaml"
+    )
+
+    set_env_vars_resources = AppendEnvironmentVariable(
+        "GZ_SIM_RESOURCE_PATH", os.path.join(rby_gazebo, "models")
+    )
+
+    ld.add_action(set_env_vars_resources)
+
+    # Launch Gazebo
+    ld.add_action(
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(gz_launch_path),
+            launch_arguments={
+                "gz_args": [world_path],
+                "on_exit_shutdown": "True",
+            }.items(),
+        ),
+    )
+
+    # Clock bridge
+    ld.add_action(
+        Node(
+            package="ros_gz_bridge",
+            executable="parameter_bridge",
+            arguments=["/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock"],
+            output="screen",
+            namespace="r2d2",
+            condition=IfCondition(ros_bridge),
+        ),
+    )
+
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    rsp_frequency = LaunchConfiguration("rsp_frequency")
+
+    # Robot State Publisher
+    ld.add_action(
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="robot_state_publisher",
+            output="both",
+            parameters=[
+                {
+                    "use_sim_time": use_sim_time,
+                    "publish_frequency": rsp_frequency,
+                    "robot_description": get_robot_description(),
+                }
+            ],
+            remappings=[
+                ("/tf", "tf"),
+                ("/tf_static", "tf_static"),
+            ],
+        ),
+    )
+
+    entity = LaunchConfiguration("entity")
+    initial_pose_x = LaunchConfiguration("initial_pose_x")
+    initial_pose_y = LaunchConfiguration("initial_pose_y")
+    initial_pose_z = LaunchConfiguration("initial_pose_z")
+    initial_pose_yaw = LaunchConfiguration("initial_pose_yaw")
+    robot_description_topic = LaunchConfiguration("robot_description_topic")
+
+    robot_spawn = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=[
+            "-name",
+            entity,
+            "-topic",
+            robot_description_topic,
+            "-x",
+            initial_pose_x,
+            "-y",
+            initial_pose_y,
+            "-z",
+            initial_pose_z,
+            "-R",
+            "0",
+            "-P",
+            "0",
+            "-Y",
+            initial_pose_yaw,
+        ],
+        output="screen",
+    )
+    # Create robot in Gazebo
+    ld.add_action(robot_spawn)
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+    )
+
+    rby_controller_spawner_head = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_trajectory_controller_head",
+            "--param-file",
+            controller_config_path,
+        ],
+    )
+
+    rby_controller_spawner_right_arm = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_trajectory_controller_right_arm",
+            "--param-file",
+            controller_config_path,
+        ],
+    )
+
+    rby_controller_spawner_left_arm = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_trajectory_controller_left_arm",
+            "--param-file",
+            controller_config_path,
+        ],
+    )
+    
+    rby_controller_spawner_left_gripper = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_trajectory_controller_left_gripper",
+            "--param-file",
+            controller_config_path,
+        ],
+    )
+    
+    rby_controller_spawner_right_gripper = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_trajectory_controller_right_gripper",
+            "--param-file",
+            controller_config_path,
+        ],
+    )
+    
+    rby_controller_spawner_torso = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_trajectory_controller_torso",
+            "--param-file",
+            controller_config_path,
+        ],
+    )
+    
+    rby_controller_spawner_wheel = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_trajectory_controller_wheel",
+            "--param-file",
+            controller_config_path,
+        ],
+    )
+    
+    
+
+    # ROS-Gazebo bridge
+    bridge_config = ReplaceString(
+        source_file=bridge_config_file_path,
+        replacements={"<entity>": entity},
+    )
+
+    ld.add_action(
+        Node(
+            package="ros_gz_bridge",
+            executable="parameter_bridge",
+            output="screen",
+            parameters=[{"config_file": bridge_config}],
+        )
+    )
+
+    ld.add_action(
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=robot_spawn,
+                on_exit=[joint_state_broadcaster_spawner],
+            )
+        )
+    )
+
+    ld.add_action(
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster_spawner,
+                on_exit=[rby_controller_spawner_head],
+            )
+        )
+    )
+    
+    ld.add_action(
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster_spawner,
+                on_exit=[rby_controller_spawner_right_arm],
+            )
+        )
+    )
+    
+
+    ld.add_action(
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster_spawner,
+                on_exit=[rby_controller_spawner_left_arm],
+            )
+        )
+    )
+    
+    ld.add_action(
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster_spawner,
+                on_exit=[rby_controller_spawner_left_gripper],
+            )
+        )
+    )
+    
+    ld.add_action(
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster_spawner,
+                on_exit=[rby_controller_spawner_right_gripper],
+            )
+        )
+    )
+    
+    ld.add_action(
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster_spawner,
+                on_exit=[rby_controller_spawner_torso],
+            )
+        )
+    )
+    
+    return ld
